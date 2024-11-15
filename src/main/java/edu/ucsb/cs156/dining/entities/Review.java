d3e53af6a791769fae0eac22702e27fa169c9b2f
Jayden <82332696+jayster7101@users.noreply.github.com>
2024-11-14 18:13:10 -0800

    Created the entitie for the Review

---
+++ src/main/java/edu/ucsb/cs156/dining/entities/Review.java
@@ -0,0 +1,38 @@
+package edu.ucsb.cs156.dining.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+/**
+ * This is a JPA entity that represents a Review, i.e. an entry
+ * that allows users to see data fields and then rate and comment on the said data, specifically about the menu and food
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity(name = "reviews")
+public class Review {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+  private String diningCommons;
+  private String meal;
+  private String menuItem;
+  private String stationName;
+  private String status;
+  private LocalDateTime dateItemServed;
+  private long userIdModerator;
+  private String moderatorComments;
+  private LocalDateTime dateCreated;
+  private LocalDateTime dateEdited;
+}
