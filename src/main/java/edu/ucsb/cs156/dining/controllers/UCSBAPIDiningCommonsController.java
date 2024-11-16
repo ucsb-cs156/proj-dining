c8371b932d20cfaa9c59fa4e2a8bf463c004fa61
Cooper Hawley <chawley@ucsb.edu>
2024-11-16 14:36:56 -0800

    Need Role User to use diningcommonsapi

--- src/main/java/edu/ucsb/cs156/dining/controllers/UCSBAPIDiningCommonsController.java
+++ src/main/java/edu/ucsb/cs156/dining/controllers/UCSBAPIDiningCommonsController.java
@@ -6,13 +6,14 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 /** Controller for UCSB Dining Commons API */
-@Tag(name = "UCSBDiningCommonsController")
+@Tag(name = "UCSBAPIDiningCommonsController")
 @RestController
 @RequestMapping("/api/diningcommons")
 @Slf4j
@@ -22,6 +23,7 @@ public class UCSBAPIDiningCommonsController {
   private UCSBAPIDiningCommonsService diningCommonsService;
 
   @Operation(summary = "Get all dining commons from UCSB API")
+  @PreAuthorize("hasRole('ROLE_USER')")
   @GetMapping("/all")
   public List<UCSBAPIDiningCommons> getAllDiningCommons() throws Exception {
     return diningCommonsService.getAllDiningCommons();
