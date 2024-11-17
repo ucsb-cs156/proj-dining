4b9f46ba249f84eea4e9b9e6b3b6c7cbf262ca6e
Riona Pampati <riona@ucsb.edu>
2024-11-16 22:26:06 -0800

    Added route to App.js for myreviews

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import AdminUsersPage from "main/pages/AdminUsersPage";
 import PlaceholderIndexPage from "main/pages/Placeholder/PlaceholderIndexPage";
 import PlaceholderCreatePage from "main/pages/Placeholder/PlaceholderCreatePage";
 import PlaceholderEditPage from "main/pages/Placeholder/PlaceholderEditPage";
+import MyReviewsPage from "main/pages/MyReviews/MyReviewsPage";
 
 import { hasRole, useCurrentUser } from "main/utils/currentUser";
 
@@ -43,6 +44,15 @@ function App() {
               exact
               path="/placeholder/create"
               element={<PlaceholderCreatePage />}
+            />
+          </>
+        )}
+        {hasRole(currentUser, "ROLE_USER") && (
+          <>
+            <Route
+              exact
+              path="/myreviews"
+              element={<MyReviewsPage />}
             />
           </>
         )}
