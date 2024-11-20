2eda8780ea9be71b3a2ac7242eb4b05dcc8ecb41
riasinha <67247334+riasinha@users.noreply.github.com>
2024-11-19 17:54:03 -0800

    added comment for improved readability

--- src/main/java/edu/ucsb/cs156/dining/repositories/DiningCommonsReviewRepository.java
+++ src/main/java/edu/ucsb/cs156/dining/repositories/DiningCommonsReviewRepository.java
@@ -5,6 +5,7 @@ import edu.ucsb.cs156.dining.entities.DiningCommonsReview;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+/*This is a repository file for DiningCommonsReview */
 
  @Repository
  public interface DiningCommonsReviewRepository extends CrudRepository<DiningCommonsReview, Long> {
