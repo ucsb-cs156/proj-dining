8bc230d815c3f7c715e217004c7fb6af26d75813
Emily Zheng <61632678+emiz39@users.noreply.github.com>
2024-11-19 18:10:31 -0800

    added reviewFixtures

---
+++ frontend/src/fixtures/reviewFixtures.js
@@ -0,0 +1,43 @@
+const reviewFixtures = {
+    oneReview: {
+      id: 1,
+      item: "Buttermilk Fried Chicken Sandwich",
+      station: "Entree Specials",
+      stars: "***",
+      reviewText: "soggy",
+      status: "Awaiting Moderation",
+      modComments: "",
+    },
+    threeReviews: [
+      {
+        id: 2,
+        item: "Black Bean Chili Rice & Garlic Chip(vgn)",
+        station: "Entree Specials",
+        stars: "**",
+        reviewText: "not very spicy",
+        status: "Approved",
+        modComments: "",
+      },
+      {
+        id: 3,
+        item: "Chicken Caesar Salad",
+        station: "Entree",
+        stars: "***",
+        reviewText: "Vote for me for AS President!",
+        status: "Rejected",
+        modComments: "Spam",
+      },
+      {
+        id: 4,
+        item: "Spicy Kale Caesar Salad (v)",
+        station: "Entree Specials",
+        stars: "**",
+        reviewText: "too spicy",
+        status: "Approved",
+        modComments: "",
+      },
+    ],
+  };
+  
+  export { reviewFixtures };
+  
