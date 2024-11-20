a402bda21758431194581cdf555eb6a6067bab49
Steven Jiang <stevenjiang@ucsb.edu>
2024-11-19 19:35:29 -0800

    Create Placeholder page for individual dining commons

---
+++ frontend/src/main/pages/DiningCommons/DiningCommonsPage.js
@@ -0,0 +1,12 @@
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+
+export default function ReviewsLookupPage() {
+  // Stryker disable all : placeholder for future implementation
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h1>Placeholder for Dining Commons Page for (diningCommonsCode)</h1>
+      </div>
+    </BasicLayout>
+  );
+}
