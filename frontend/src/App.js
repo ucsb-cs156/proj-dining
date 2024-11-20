58b411640f3765b145cb19fdb9f36dfde6f2ecac
salshriaan <salshriaan@ucsb.edu>
2024-11-19 17:41:41 -0800

    sa - added placeholder index page for myreviews and tested

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,8 @@ import PlaceholderIndexPage from "main/pages/Placeholder/PlaceholderIndexPage";
 import PlaceholderCreatePage from "main/pages/Placeholder/PlaceholderCreatePage";
 import PlaceholderEditPage from "main/pages/Placeholder/PlaceholderEditPage";
 
+import MyReviewsIndexPage from 'main/pages/MyReviews/MyReviewsIndexPage';
+
 import { hasRole, useCurrentUser } from "main/utils/currentUser";
 
 import "bootstrap/dist/css/bootstrap.css";
@@ -36,6 +38,13 @@ function App() {
             <Route exact path="/ucsbdates" element={<UCSBDatesIndexPage />} />
           </>
         )}
+
+        {hasRole(currentUser, "ROLE_USER") && (
+          <>
+            <Route exact path="/myreviews" element={<MyReviewsIndexPage />} />
+          </>
+        )}
+
         {hasRole(currentUser, "ROLE_ADMIN") && (
           <>
             <Route
