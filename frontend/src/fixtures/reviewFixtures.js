33639cbc5c430bc9b6420b8c186d176563d68934
Emily Zheng <61632678+emiz39@users.noreply.github.com>
2024-11-19 22:39:09 -0800

    formatted fixtures

--- frontend/src/fixtures/reviewFixtures.js
+++ frontend/src/fixtures/reviewFixtures.js
@@ -1,43 +1,42 @@
 const reviewFixtures = {
-    oneReview: {
+  oneReview: {
+    id: 1,
+    item: "Buttermilk Fried Chicken Sandwich",
+    station: "Entree Specials",
+    stars: "***",
+    reviewText: "soggy",
+    status: "Awaiting Moderation",
+    modComments: "",
+  },
+  threeReviews: [
+    {
       id: 1,
-      item: "Buttermilk Fried Chicken Sandwich",
+      item: "Black Bean Chili Rice & Garlic Chip(vgn)",
       station: "Entree Specials",
-      stars: "***",
-      reviewText: "soggy",
-      status: "Awaiting Moderation",
+      stars: "**",
+      reviewText: "not very spicy",
+      status: "Approved",
       modComments: "",
     },
-    threeReviews: [
-      {
-        id: 2,
-        item: "Black Bean Chili Rice & Garlic Chip(vgn)",
-        station: "Entree Specials",
-        stars: "**",
-        reviewText: "not very spicy",
-        status: "Approved",
-        modComments: "",
-      },
-      {
-        id: 3,
-        item: "Chicken Caesar Salad",
-        station: "Entree",
-        stars: "***",
-        reviewText: "Vote for me for AS President!",
-        status: "Rejected",
-        modComments: "Spam",
-      },
-      {
-        id: 4,
-        item: "Spicy Kale Caesar Salad (v)",
-        station: "Entree Specials",
-        stars: "**",
-        reviewText: "too spicy",
-        status: "Approved",
-        modComments: "",
-      },
-    ],
-  };
-  
-  export { reviewFixtures };
-  
+    {
+      id: 2,
+      item: "Chicken Caesar Salad",
+      station: "Entree",
+      stars: "***",
+      reviewText: "Vote for me for AS President!",
+      status: "Rejected",
+      modComments: "Spam",
+    },
+    {
+      id: 3,
+      item: "Spicy Kale Caesar Salad (v)",
+      station: "Entree Specials",
+      stars: "**",
+      reviewText: "too spicy",
+      status: "Approved",
+      modComments: "",
+    },
+  ],
+};
+
+export { reviewFixtures };
