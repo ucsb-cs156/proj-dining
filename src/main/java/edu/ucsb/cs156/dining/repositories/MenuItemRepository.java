7f26a89cfc41c0728a89e19e18edd0b27fdcce34
oviyaseeniraj <66218817+oviyaseeniraj@users.noreply.github.com>
2024-11-20 14:40:29 -0800

    added menuitem database entity repo and changelog, modified returns menuitems endpoint to add to database if item nonexistent in database

---
+++ src/main/java/edu/ucsb/cs156/dining/repositories/MenuItemRepository.java
@@ -0,0 +1,27 @@
+package edu.ucsb.cs156.dining.repositories;
+
+import edu.ucsb.cs156.dining.entities.MenuItem;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+/**
+ * The UserRepository is a repository for User entities.
+ */
+@Repository
+public interface MenuItemRepository extends CrudRepository<MenuItem, Long> {
+
+    /**
+     * This method returns a Menu Item.
+     * 
+     * @param diningCommonsCode code of dining commons
+     * @param meal meal of food item
+     * @param name name of food item
+     * @param station station of food item
+     * 
+     * @return Optional of MenuItem
+     */
+    Optional<MenuItem> findByDiningCommonsCodeAndMealAndNameAndStation(String diningCommonsCode, String meal, String name, String station);
+ 
+}
