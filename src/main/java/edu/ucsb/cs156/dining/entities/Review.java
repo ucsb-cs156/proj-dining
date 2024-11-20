1aec55ef20585a3e87e7449b97bd3e8922c8d267
Jayden <82332696+jayster7101@users.noreply.github.com>
2024-11-19 18:15:20 -0800

    changed the formatting

--- src/main/java/edu/ucsb/cs156/dining/entities/Review.java
+++ src/main/java/edu/ucsb/cs156/dining/entities/Review.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,26 +12,37 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
-/**
- * This is a JPA entity that represents a Review, i.e. an entry
- * that allows users to see data fields and then rate and comment on the said data, specifically about the menu and food
- */
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Entity(name = "reviews")
 public class Review {
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private long id;
-  private long studentId;
-  private long itemId;
-  private LocalDateTime dateItemServed;
-  private String status;
-  private long userIdModerator;
-  private String moderatorComments;
-  private LocalDateTime dateCreated;
-  private LocalDateTime dateEdited;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private long studentId;
+
+    @Column(nullable = false)
+    private String itemId;
+
+    @Column(nullable = false)
+    private LocalDateTime dateItemServed;
+
+    @Column(nullable = false, columnDefinition = "VARCHAR(255) DEFAULT 'Awaiting Moderation'")
+    private String status = "Awaiting Moderation";
+
+    @Column(columnDefinition = "VARCHAR(255) DEFAULT NULL")
+    private String userIdModerator;
+
+    @Column(columnDefinition = "VARCHAR(255) DEFAULT NULL")
+    private String moderatorComments; 
+
+    @Column(nullable = false)
+    private LocalDateTime dateCreated;
+
+    @Column(nullable = false)
+    private LocalDateTime dateEdited;
 }
