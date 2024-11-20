d32cac32ebe4cb208ffdef175b75ffa3ba915ce3
Hao Huan Tang <haohuantang@ucsb.edu>
2024-11-19 17:53:09 -0800

    Added fixtures for meals table

---
+++ frontend/src/fixtures/mealFixtures.js
@@ -0,0 +1,24 @@
+const mealFixtures = {
+    oneMeal: {
+      name: "Late Night",
+      code: "late-night",
+    },
+  
+    threeMeals: [
+      {
+        name: "Late Night1",
+        code: "late-night1",
+      },
+      {
+        name: "Late Night2",
+        code: "late-night2",
+      },
+      {
+        name: "Late Night3",
+        code: "late-night3",
+      },
+    ],
+  };
+  
+  export { mealFixtures };
+  
