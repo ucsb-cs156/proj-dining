ee480a98827bf98c68725e42c175b20097adc934
Aman Desai <amandesai@ucsb.edu>
2024-11-20 17:17:22 -0800

    Fixed git action errors (hopefully)

--- frontend/src/tests/pages/HomePage.test.js
+++ frontend/src/tests/pages/HomePage.test.js
@@ -1,4 +1,4 @@
-import { render, screen, waitFor, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import HomePage from "main/pages/HomePage";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
@@ -9,33 +9,6 @@ import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
 import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
 import axios from "axios";
 import AxiosMockAdapter from "axios-mock-adapter";
-
-/* describe("HomePage tests", () => {
-  const axiosMock = new AxiosMockAdapter(axios);
-  axiosMock
-    .onGet("/api/currentUser")
-    .reply(200, apiCurrentUserFixtures.userOnly);
-  axiosMock
-    .onGet("/api/systemInfo")
-    .reply(200, systemInfoFixtures.showingNeither);
-
-  const queryClient = new QueryClient();
-  test("renders without crashing", async () => {
-    render(
-      <QueryClientProvider client={queryClient}>
-        <MemoryRouter>
-          <HomePage />
-        </MemoryRouter>
-      </QueryClientProvider>,
-    );
-    await screen.findByText(/Dining Commons/);
-    await screen.findByText(/Code/);
-    await screen.findByText(/Name/);
-    expect(
-      await screen.findByTestId("DiningCommonsTable-header-group-0"),
-    ).toBeInTheDocument();
-  });
-}); */
 
 const axiosMock = new AxiosMockAdapter(axios);
 
