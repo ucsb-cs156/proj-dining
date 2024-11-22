959715180b80c1f1728ebfb86b6a2be1c409306f
Kailai <kailai@ucsb.edu>
2024-11-21 17:24:18 -0800

    add description

--- src/main/java/edu/ucsb/cs156/dining/controllers/UCSBAPIMenuController.java
+++ src/main/java/edu/ucsb/cs156/dining/controllers/UCSBAPIMenuController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
