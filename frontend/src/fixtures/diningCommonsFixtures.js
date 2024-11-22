a6e1030f716612a1ca1c2e577bf4a08c7212f590
Anvitha Kosuri <anvithakosuri@ucsb.edu>
2024-11-21 17:58:19 -0800

    added dining commons fixtures

---
+++ frontend/src/fixtures/diningCommonsFixtures.js
@@ -0,0 +1,50 @@
+const diningCommonsFixtures = {
+    oneDate: {
+        name: "steve",
+        code: "help world",
+        hasDiningCam: "true",
+        hasSackMeal: "true",
+        hasTakeoutMeal: "false",
+        location: {
+            latitude: 0,
+            longitude: 0
+        }
+    },
+    threeDates: [
+        {
+            name: "mike",
+            code: "xyz",
+            hasDiningCam: "true",
+            hasSackMeal: "true",
+            hasTakeoutMeal: "false",
+            location: {
+                latitude: 0,
+                longitude: 0
+            }
+        },
+        {
+            name: "bob",
+            code: "team 13",
+            hasDiningCam: "true",
+            hasSackMeal: "true",
+            hasTakeoutMeal: "false",
+            location: {
+                latitude: 0,
+                longitude: 0
+            }
+        },
+        {
+            name: "jake",
+            code: "testing",
+            hasDiningCam: "true",
+            hasSackMeal: "true",
+            hasTakeoutMeal: "false",
+            location: {
+                latitude: 0,
+                longitude: 0
+            }
+        },
+    ],
+};
+
+export { diningCommonsFixtures };
