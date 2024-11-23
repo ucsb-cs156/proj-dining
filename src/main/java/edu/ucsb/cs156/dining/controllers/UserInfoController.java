bc2dab099d375277de997b591329ac0286f4831a
cs11111111 <song991@ucsb.edu>
2024-11-23 15:16:23 -0800

    moved over controllers

--- src/main/java/edu/ucsb/cs156/dining/controllers/UserInfoController.java
+++ src/main/java/edu/ucsb/cs156/dining/controllers/UserInfoController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
-
 /**
  * This is a REST controller for getting information about the current user.
  */
