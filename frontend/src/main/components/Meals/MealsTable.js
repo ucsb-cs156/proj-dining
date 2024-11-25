1aebc778816eeeb1e3784e752c189550bc7713d7
Phill Conrad <pconrad@cs.ucsb.edu>
2024-11-24 16:28:24 -0800

    Update frontend/src/main/components/Meals/MealsTable.js

--- frontend/src/main/components/Meals/MealsTable.js
+++ frontend/src/main/components/Meals/MealsTable.js
@@ -2,7 +2,6 @@ import React from "react";
 import OurTable from "main/components/OurTable";
 
 export default function MealsTable({ meals }) {
-  // Stryker disable all : hard to test for query caching
 
 
 
