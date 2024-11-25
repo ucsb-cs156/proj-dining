176cd4a3f02f134e8d57a00d68a88a10a9ff41d7
Va1a <n/a>
2024-11-24 16:56:58 -0800

    Fix the /{date-time}/{dining-commons-code}/{meal} endpoint. This consisted of changing the Controller's JSON imports to use org.json imports, which meant changing the pom.xml file to add org.json as a dependency. Next, the database had to be updated to use DINING_COMMONS_CODE as the column name. After that, the endpoint functioned as expected.

--- src/main/java/edu/ucsb/cs156/dining/controllers/DiningCommonsController.java
+++ src/main/java/edu/ucsb/cs156/dining/controllers/DiningCommonsController.java
@@ -8,8 +8,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.configurationprocessor.json.JSONArray;
-import org.springframework.boot.configurationprocessor.json.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
