0378420725e48c4d55bde17abf23d95dbb3bd6d7
Phill Conrad <pconrad@cs.ucsb.edu>
2024-11-24 16:28:02 -0800

    Update frontend/src/main/components/Meals/MealsTable.js

--- frontend/src/main/components/Meals/MealsTable.js
+++ frontend/src/main/components/Meals/MealsTable.js
@@ -4,7 +4,6 @@ import OurTable from "main/components/OurTable";
 export default function MealsTable({ meals }) {
   // Stryker disable all : hard to test for query caching
 
-  // Stryker restore all
 
   // Stryker disable next-line all : TODO try to make a good test for this
 
