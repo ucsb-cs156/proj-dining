ed199544ab1c39c270a189ff005d48c00818772e
Emily Zheng <61632678+emiz39@users.noreply.github.com>
2024-11-26 18:10:18 -0800

    deleted onDeleteSuccess

--- frontend/src/main/utils/ReviewUtils.js
+++ frontend/src/main/utils/ReviewUtils.js
@@ -1,10 +1,3 @@
-import { toast } from "react-toastify";
-
-export function onDeleteSuccess(message) {
-  console.log(message);
-  toast(message);
-}
-
 export function cellToAxiosParamsDelete(cell) {
   return {
     url: "/api/reviews",
