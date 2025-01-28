8c11542e657c5c3c93af40cfd0c5ad85d7c1b362
Daniel Jensen <djensen2@outlook.com>
2025-01-27 21:48:14 -0800

    dj - forgot to run format

--- frontend/src/tests/utils/currentUser.test.js
+++ frontend/src/tests/utils/currentUser.test.js
@@ -226,25 +226,15 @@ describe("utils/currentUser tests", () => {
     test("hasRole falls back correctly with various data missing", async () => {
       expect(hasRole(null, "ROLE_USER")).toBeFalsy();
       expect(hasRole({}, "ROLE_USER")).toBeFalsy();
-      expect(hasRole({ data: null } , "ROLE_USER")).toBeFalsy();
+      expect(hasRole({ data: null }, "ROLE_USER")).toBeFalsy();
+      expect(hasRole({ data: { root: null } }, "ROLE_USER")).toBeFalsy();
       expect(
-        hasRole({ data: { root: null } }, "ROLE_USER"),
+        hasRole({ data: { root: { rolesList: null } } }, "ROLE_USER"),
       ).toBeFalsy();
       expect(
-        hasRole(
-          { data: { root: { rolesList: null } } },
-          "ROLE_USER",
-        ),
-      ).toBeFalsy();
-      expect(
-        hasRole(
-           { data: { root: { rolesList: [] } } },
-          "ROLE_USER",
-        ),
+        hasRole({ data: { root: { rolesList: [] } } }, "ROLE_USER"),
       ).toBeFalsy();
     });
-      expect(
-          hasRole({root: {rolesList: null}})
-      ).toBeFalsy();
+    expect(hasRole({ root: { rolesList: null } })).toBeFalsy();
   });
 });
