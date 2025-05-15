172bf2d2eec25513b75c6b84f0ae25f8743d61f4
Aryan Chopra <aryanchopra@ucsb.edu>
2025-05-14 22:06:17 -0700

    aryanchopra - Added placeholder page and test for MyReviews Edit Page

---
+++ frontend/src/main/pages/MyReviews/MyReviewsEditPage.js
@@ -0,0 +1,12 @@
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+
+export default function MyReviewsIndexPage() {
+  // Stryker disable all : placeholder for future implementation
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h1>Index page not yet implemented</h1>
+      </div>
+    </BasicLayout>
+  );
+}
