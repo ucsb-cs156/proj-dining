39f3c4cb6c4cf99ffda3b50842d6dbe6f32b6f7b
Sanjana Jilla <sjilla@ucsb.edu>
2025-05-14 18:36:22 -0700

    Fixed unused vars issues

--- frontend/src/main/components/Review/ReviewTable.js
+++ frontend/src/main/components/Review/ReviewTable.js
@@ -1,10 +1,8 @@
 import React from "react";
 import OurTable, { ButtonColumn } from "main/components/OurTable";
-import { hasRole } from "main/utils/currentUser";
 
 export default function ReviewTable({
   reviews,
-  currentUser,
   userOptions = false,
   moderatorOptions = false,
   onEdit,
