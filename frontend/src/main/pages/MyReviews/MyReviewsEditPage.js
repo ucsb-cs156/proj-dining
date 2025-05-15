50f0c69799a73c682ca0b04bbf15e701482381df
Aryan Chopra <aryanchopra@ucsb.edu>
2025-05-14 22:11:50 -0700

    aryanchopra - Fixed naming errors in the placeholder file and test

--- frontend/src/main/pages/MyReviews/MyReviewsEditPage.js
+++ frontend/src/main/pages/MyReviews/MyReviewsEditPage.js
@@ -1,11 +1,11 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 
-export default function MyReviewsIndexPage() {
+export default function MyReviewsEditPage() {
   // Stryker disable all : placeholder for future implementation
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Index page not yet implemented</h1>
+        <h1>Edit page not yet implemented</h1>
       </div>
     </BasicLayout>
   );
