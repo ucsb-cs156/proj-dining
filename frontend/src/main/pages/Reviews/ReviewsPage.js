18f432a5d8f4584038fb72e30d9549367b63f9b8
Tiffany Gu <tiffanygu@ucsb.edu>
2025-05-19 17:47:43 -0700

    edited placeholder page, adding useParams so the id can be displayed

--- frontend/src/main/pages/Reviews/ReviewsPage.js
+++ frontend/src/main/pages/Reviews/ReviewsPage.js
@@ -1,10 +1,12 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import { useParams } from "react-router-dom";
 
 export default function ReviewsPage() {
+  const { id } = useParams(); 
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Reviews for Menu Item</h1>
+        <h1>Reviews for Menu Item {id}</h1>
         <p>Coming soon!</p>
       </div>
     </BasicLayout>
