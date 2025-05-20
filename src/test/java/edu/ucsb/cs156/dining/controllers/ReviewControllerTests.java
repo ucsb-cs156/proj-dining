4f7980193d9af534a7447032c09a78ac7c695add
matthew <linghe@ucsb.edu>
2025-05-19 18:19:42 -0700

    mz-new-tests

--- src/test/java/edu/ucsb/cs156/dining/controllers/ReviewControllerTests.java
+++ src/test/java/edu/ucsb/cs156/dining/controllers/ReviewControllerTests.java
@@ -1030,4 +1030,40 @@ public void test_validComment_on_creating_new_review() throws Exception {
     verify(reviewRepository).save(any(Review.class));
     assertEquals(jsonReview, responseJson);
 }
+
+@WithMockUser(roles = {"USER"})
+@Test
+public void review_with_null_comment_is_auto_approved() throws Exception {
+    // Arrange
+    LocalDateTime now = LocalDateTime.now();
+    User user = currentUserService.getUser();
+    MenuItem menuItem = MenuItem.builder().id(1L).build();
+
+    Review reviewSaved = Review.builder()
+            .id(21L)
+            .dateCreated(now)
+            .dateEdited(now)
+            .itemsStars(4L)
+            .reviewerComments(null)
+            .dateItemServed(LocalDateTime.of(2024, 5, 2, 11, 30))
+            .reviewer(user)
+            .status(ModerationStatus.APPROVED)
+            .item(menuItem)
+            .build();
+
+    when(reviewRepository.save(any(Review.class))).thenReturn(reviewSaved);
+
+    // Act
+    MvcResult response = mockMvc.perform(
+                    post("/api/reviews/post?itemId=1&itemsStars=4&dateItemServed=2024-05-02T11:30:00")
+                            .with(csrf()))
+            .andExpect(status().isOk())
+            .andReturn();
+
+    String expectedJson = mapper.writeValueAsString(reviewSaved);
+    String responseJson = response.getResponse().getContentAsString();
+
+    verify(reviewRepository).save(any(Review.class));
+    assertEquals(expectedJson, responseJson);
+}
 }
