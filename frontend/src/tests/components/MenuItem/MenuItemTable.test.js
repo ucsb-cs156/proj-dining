9ec8c0d8904e6d7d225781622062e21d922c222e
Montgomery Forde <montgomery@ucsb.edu>
2025-05-20 09:43:30 -0700

    added tests for avgrating

--- frontend/src/tests/components/MenuItem/MenuItemTable.test.js
+++ frontend/src/tests/components/MenuItem/MenuItemTable.test.js
@@ -53,6 +53,9 @@ describe("MenuItemTable Tests", () => {
     expect(
       screen.queryByTestId("MenuItemTable-cell-row-0-col-Review Item-button"),
     ).not.toBeInTheDocument();
+    expect(
+      screen.getByTestId("MenuItemTable-header-avgRating")
+    ).toHaveTextContent("Avg Rating");
   });
   test("Renders 5 Menu Items Correctly and Reviews link is correct", async () => {
     let fiveMenuItems = menuItemFixtures.fiveMenuItems;
@@ -72,6 +75,9 @@ describe("MenuItemTable Tests", () => {
       expect(
         screen.getByTestId(`MenuItemTable-cell-row-${i}-col-station`),
       ).toHaveTextContent(fiveMenuItems[i].station);
+      expect(
+        screen.getByTestId(`MenuItemTable-cell-row-${i}-col-avgRating`)
+      ).toHaveTextContent("—");
       // Check Reviews link
       const reviewsCell = screen.getByTestId(
         `MenuItemTable-cell-row-${i}-col-id`,
@@ -126,5 +132,46 @@ describe("MenuItemTable Tests", () => {
     expect(
       screen.queryByTestId("MenuItemTable-cell-row-0-col-Review Item-button"),
     ).not.toBeInTheDocument();
+  });
+
+   test("Avg Rating cell shows one decimal", () => {
+    const data = [
+      {
+        ...menuItemFixtures.oneMenuItem[0],
+        avgRating: 3.456,
+      },
+    ];
+    render(
+      <MemoryRouter>
+        <MenuItemTable
+          menuItems={data}
+          currentUser={currentUserFixtures.notLoggedIn}
+        />
+      </MemoryRouter>,
+    );
+    expect(
+      screen.getByTestId("MenuItemTable-cell-row-0-col-avgRating"),
+    ).toHaveTextContent("3.5");
+  });
+
+  test("Avg Rating cell shows '—' when avgRating is null", () => {
+    const data = [
+      {
+        ...menuItemFixtures.oneMenuItem[0],
+        avgRating: null,
+      },
+    ];
+    render(
+      <MemoryRouter>
+        <MenuItemTable
+          menuItems={data}
+          currentUser={currentUserFixtures.notLoggedIn}
+        />
+      </MemoryRouter>,
+    );
+
+    expect(
+      screen.getByTestId("MenuItemTable-cell-row-0-col-avgRating"),
+    ).toHaveTextContent("—");
   });
 });
