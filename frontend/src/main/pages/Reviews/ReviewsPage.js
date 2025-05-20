b392e53b56a09e3cba0cc5a776d66688b4b242f3
Tiffany Gu <tiffanygu@ucsb.edu>
2025-05-19 18:15:47 -0700

    changed reviewspage placeholder so that item id is displayed

--- frontend/src/main/pages/Reviews/ReviewsPage.js
+++ frontend/src/main/pages/Reviews/ReviewsPage.js
@@ -2,11 +2,11 @@ import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 import { useParams } from "react-router-dom";
 
 export default function ReviewsPage() {
-  const {id} = useParams(); 
+  const {itemid} = useParams(); 
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Reviews for Menu Item {id}</h1>
+        <h1>Reviews for Menu Item {itemid}</h1>
         <p>Coming soon!</p>
       </div>
     </BasicLayout>
