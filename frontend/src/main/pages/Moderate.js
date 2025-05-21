daaf2e12d1099fc7365bbf290c87cb507856cbfe
Yarwin Liu <yarwinliu@ucsb.edu>
2025-05-20 18:45:10 -0700

    yl - placeholder

--- frontend/src/main/pages/Moderate.js
+++ frontend/src/main/pages/Moderate.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useCurrentUser, hasRole } from "main/utils/currentUser";
 import { Navigate } from "react-router-dom";
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
-import { _Table, _Button } from "react-bootstrap";
+import { Table, _Button } from "react-bootstrap";
 import AliasTable from "../components/Alias/AliasTable";
 import { useBackend } from "../utils/useBackend";
 
@@ -21,6 +21,7 @@ const Moderate = () => {
   }
 
   // TODO: Replace this placeholder with the actual Reviews Table component when available
+
   return (
     <BasicLayout>
       <div className="pt-2">
@@ -30,8 +31,29 @@ const Moderate = () => {
         <AliasTable alias={data} />
         <br />
         <h4> Reviews Table </h4>
-        {/* Placeholder: Using AliasTable for Reviews Table until ReviewsTable component is implemented */}
-        <AliasTable alias={data} />
+        {/* Placeholder: until ReviewsTable component is implemented */}
+        {/* Stryker disable all */}
+        <Table striped bordered hover>
+            <thead>
+                <tr>
+                    <th>Score</th>
+                    <th>Comments</th>
+                    <th>Date Served</th>
+                    <th>Item Name</th>
+                    <th>Edit</th>
+                    <th>Delete</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td colSpan="6" className="text-center">
+                        Reviews Table Placeholder
+                    </td>
+                </tr>
+            </tbody>
+        </Table>
+        {/* Stryker restore all */}
+        
       </div>
     </BasicLayout>
   );
