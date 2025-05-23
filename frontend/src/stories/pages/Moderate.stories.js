4cbccc6ec5b02d5ad38cb9eae994b5cdcd30eec0
Renbo2004 <renbo@ucsb.edu>
2025-05-23 16:59:27 -0700

    Create Moderate.stories.js

---
+++ frontend/src/stories/pages/Moderate.stories.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { http, HttpResponse } from "msw";
+import Moderate from "main/pages/Moderate";
+import aliasFixtures from "fixtures/aliasFixtures";
+import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
+
+export default {
+  title: "pages/Moderate",
+  component: Moderate,
+};
+
+const Template = () => <Moderate />;
+
+export const LoggedOut = Template.bind({});
+LoggedOut.parameters = {
+  msw: {
+    handlers: [
+      http.get("/api/currentUser", () => {
+        return HttpResponse.json(null, { status: 403 });
+      }),
+    ],
+  },
+};
+
+export const NoAdminRole = Template.bind({});
+NoAdminRole.parameters = {
+  msw: {
+    handlers: [
+      http.get("/api/currentUser", () => {
+        return HttpResponse.json(apiCurrentUserFixtures.userOnly);
+      }),
+    ],
+  },
+};
+
+export const AdminView = Template.bind({});
+AdminView.parameters = {
+  msw: {
+    handlers: [
+      http.get("/api/currentUser", () => {
+        return HttpResponse.json(apiCurrentUserFixtures.adminUser);
+      }),
+      http.get("/api/admin/usersWithProposedAlias", () => {
+        return HttpResponse.json(aliasFixtures.threeAlias);
+      }),
+    ],
+  },
+};
