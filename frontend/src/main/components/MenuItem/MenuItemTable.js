c693327b6b0c846551879cd26f978ad4a8a19175
Selena Zeng <selenazeng@ucsb.edu>
2025-05-23 21:13:29 -0700

    addressed code review - Post Review button navigates to Placeholder page instead of popup

--- frontend/src/main/components/MenuItem/MenuItemTable.js
+++ frontend/src/main/components/MenuItem/MenuItemTable.js
@@ -1,11 +1,16 @@
 import OurTable, { ButtonColumn } from "../OurTable";
 import { hasRole } from "../../utils/currentUser";
+import { useNavigate } from "react-router-dom";
 
 export default function MenuItemTable({ menuItems, currentUser }) {
   const testid = "MenuItemTable";
+  const navigate = useNavigate();
+
   const reviewCallback = async (_cell) => {
-    alert("Reviews coming soon!");
+    const itemId = _cell.row.original.id;
+    navigate(`/reviews/post/${itemId}`);
   };
+
   const columns = [
     {
       Header: "Item Name",
@@ -16,11 +21,13 @@ export default function MenuItemTable({ menuItems, currentUser }) {
       accessor: "station",
     },
   ];
+
   if (hasRole(currentUser, "ROLE_USER")) {
     columns.push(
-      ButtonColumn("Review Item", "warning", reviewCallback, testid),
+      ButtonColumn("Review Item", "warning", reviewCallback, testid)
     );
   }
 
   return <OurTable columns={columns} data={menuItems} testid={testid} />;
 }
+
