1e4a43e987d5bc6d71b856fb4d6dcf988db5a6c9
Montgomery Forde <montgomery@ucsb.edu>
2025-05-25 19:31:02 -0700

    removed react-router-dom imports

--- frontend/src/main/pages/Reviews/ReviewsForMenuItemPage.js
+++ frontend/src/main/pages/Reviews/ReviewsForMenuItemPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams } from "react-router";
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 
 export default function ReviewsForMenuItemPage() {
