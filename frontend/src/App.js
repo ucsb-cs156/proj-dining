e1ac6568b9c459c617ef0ec18104008edef8458e
Montgomery Forde <montgomery@ucsb.edu>
2025-05-25 19:27:46 -0700

    fixed duplicated import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,7 +20,6 @@ import { hasRole, useCurrentUser } from "main/utils/currentUser";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "react-toastify/dist/ReactToastify.css";
-import MenuItemPage from "main/pages/MenuItem/MenuItemPage";
 import ReviewsForMenuItemPage from "main/pages/Reviews/ReviewsForMenuItemPage";
 
 function App() {
