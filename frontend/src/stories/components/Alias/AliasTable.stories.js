700d16c40dfbb3711a31b540a2acf12d8b325d62
Renbo2004 <renbo@ucsb.edu>
2025-05-27 11:58:53 -0700

    Update AliasTable.stories.js

--- frontend/src/stories/components/Alias/AliasTable.stories.js
+++ frontend/src/stories/components/Alias/AliasTable.stories.js
@@ -2,13 +2,13 @@ import React from "react";
 import AliasTable from "main/components/Alias/AliasTable";
 import usersFixtures from "fixtures/usersFixtures";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { action } from "@storybook/addon-actions";
 
 export default {
   title: "components/Alias/AliasTable",
   component: AliasTable,
   decorators: [
     (Story) => {
-      // Wrap stories with React Query context
       const queryClient = new QueryClient();
       return (
         <QueryClientProvider client={queryClient}>
@@ -23,10 +23,14 @@ const Template = (args) => <AliasTable {...args} />;
 
 export const EmptyTable = Template.bind({});
 EmptyTable.args = {
-  alias: [],
+  aliases: [],
+  onApprove: action("onApprove"),
+  onReject: action("onReject"),
 };
 
 export const ThreeUsers = Template.bind({});
 ThreeUsers.args = {
-  alias: usersFixtures.threeUsers,
+  aliases: usersFixtures.threeUsers,
+  onApprove: action("onApprove"),
+  onReject: action("onReject"),
 };
