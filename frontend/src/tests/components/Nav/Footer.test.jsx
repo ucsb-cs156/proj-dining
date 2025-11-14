b4984b7180f00fe3e06f21f78d64f412bf760d64
IanSheinis <iansheinis@gmail.com>
2025-11-13 18:40:04 -0800

    Added test for footer

--- frontend/src/tests/components/Nav/Footer.test.jsx
+++ frontend/src/tests/components/Nav/Footer.test.jsx
@@ -4,14 +4,22 @@ import Footer from "main/components/Nav/Footer";
 describe("Footer tests", () => {
   test("renders correctly", async () => {
     render(<Footer />);
-    const expectedText =
-      "This is a sample webapp using React with a Spring Boot backend. See the source code on Github.";
-    expect(screen.getByTestId("Footer").textContent).toBe(expectedText);
-    // await screen.findByText(
-    //   /This is a sample webapp using React with a Spring Boot backend./,
-    // );
-    // await screen.findByText(/source code on/);
-    // const githubLink = screen.getByText("Github");
-    // expect(githubLink).toBeInTheDocument();
+    
+    expect(screen.getByText(/This app is a class project of/)).toBeInTheDocument();
+    expect(screen.getByText(/CMPSC 156/)).toBeInTheDocument();
+    expect(screen.getByText(/at/)).toBeInTheDocument();
+    expect(screen.getByText(/UCSB/)).toBeInTheDocument();
+    
+    const cmpsc156Link = screen.getByRole('link', { name: /CMPSC 156/i });
+    expect(cmpsc156Link).toHaveAttribute('href', 'https://ucsb-cs156.github.io/');
+    
+    const ucsbLink = screen.getByRole('link', { name: /UCSB/i });
+    expect(ucsbLink).toHaveAttribute('href', 'https://ucsb.edu/');
+    
+    const githubLink = screen.getByRole('link', { name: /GitHub/i });
+    expect(githubLink).toHaveAttribute('href', 'https://github.com/ucsb-cs156/proj-courses');
+    
+    const diningLink = screen.getByRole('link', { name: /here/i });
+    expect(diningLink).toHaveAttribute('href', 'https://apps.dining.ucsb.edu/menu/day');
   });
 });
