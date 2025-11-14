73df4483732e647eeeebfbbcb16867c48f9dd7c1
IanSheinis <iansheinis@gmail.com>
2025-11-13 18:55:37 -0800

    Ran prettier

--- frontend/src/main/components/Nav/Footer.jsx
+++ frontend/src/main/components/Nav/Footer.jsx
@@ -5,13 +5,12 @@ export default function Footer() {
     <footer className="bg-light pt-3 pt-md-4 pb-4 pb-md-5" data-testid="Footer">
       <Container>
         <p>
-          This app is a class project of{' '}
-          <a href="https://ucsb-cs156.github.io/">CMPSC 156</a>
-          {' '}at{' '}
-          <a href="https://ucsb.edu/">UCSB</a>. Check out the source code on{' '}
+          This app is a class project of{" "}
+          <a href="https://ucsb-cs156.github.io/">CMPSC 156</a> at{" "}
+          <a href="https://ucsb.edu/">UCSB</a>. Check out the source code on{" "}
           <a href="https://github.com/ucsb-cs156/proj-courses">GitHub</a>! This
           is not an official source of UCSB dining commons information. An
-          official source can be found{' '}
+          official source can be found{" "}
           <a href="https://apps.dining.ucsb.edu/menu/day">here</a>.
         </p>
       </Container>
--- frontend/src/tests/components/Nav/Footer.test.jsx
+++ frontend/src/tests/components/Nav/Footer.test.jsx
@@ -4,22 +4,33 @@ import Footer from "main/components/Nav/Footer";
 describe("Footer tests", () => {
   test("renders correctly", async () => {
     render(<Footer />);
-    
-    expect(screen.getByText(/This app is a class project of/)).toBeInTheDocument();
+
+    expect(
+      screen.getByText(/This app is a class project of/),
+    ).toBeInTheDocument();
     expect(screen.getByText(/CMPSC 156/)).toBeInTheDocument();
     expect(screen.getByText(/at/)).toBeInTheDocument();
     expect(screen.getByText(/UCSB/)).toBeInTheDocument();
-    
-    const cmpsc156Link = screen.getByRole('link', { name: /CMPSC 156/i });
-    expect(cmpsc156Link).toHaveAttribute('href', 'https://ucsb-cs156.github.io/');
-    
-    const ucsbLink = screen.getByRole('link', { name: /UCSB/i });
-    expect(ucsbLink).toHaveAttribute('href', 'https://ucsb.edu/');
-    
-    const githubLink = screen.getByRole('link', { name: /GitHub/i });
-    expect(githubLink).toHaveAttribute('href', 'https://github.com/ucsb-cs156/proj-courses');
-    
-    const diningLink = screen.getByRole('link', { name: /here/i });
-    expect(diningLink).toHaveAttribute('href', 'https://apps.dining.ucsb.edu/menu/day');
+
+    const cmpsc156Link = screen.getByRole("link", { name: /CMPSC 156/i });
+    expect(cmpsc156Link).toHaveAttribute(
+      "href",
+      "https://ucsb-cs156.github.io/",
+    );
+
+    const ucsbLink = screen.getByRole("link", { name: /UCSB/i });
+    expect(ucsbLink).toHaveAttribute("href", "https://ucsb.edu/");
+
+    const githubLink = screen.getByRole("link", { name: /GitHub/i });
+    expect(githubLink).toHaveAttribute(
+      "href",
+      "https://github.com/ucsb-cs156/proj-courses",
+    );
+
+    const diningLink = screen.getByRole("link", { name: /here/i });
+    expect(diningLink).toHaveAttribute(
+      "href",
+      "https://apps.dining.ucsb.edu/menu/day",
+    );
   });
 });
