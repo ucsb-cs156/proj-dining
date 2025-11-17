4d9d6ccdaa3697ae826a45b955a52e77d803cc69
Jasmine Tan <jasminetan@ucsb.edu>
2025-11-16 16:13:34 -0800

    jt - updated formatting for footer and test

--- frontend/src/tests/components/Nav/Footer.test.jsx
+++ frontend/src/tests/components/Nav/Footer.test.jsx
@@ -7,11 +7,5 @@ describe("Footer tests", () => {
     const expectedText =
       "This app is a class project of CMPSC 156 at UCSB. Check out the source code on GitHub! This is not an official source of UCSB dining commons information. An official source can be found here.";
     expect(screen.getByTestId("Footer").textContent).toBe(expectedText);
-    // await screen.findByText(
-    //   /This is a sample webapp using React with a Spring Boot backend./,
-    // );
-    // await screen.findByText(/source code on/);
-    // const githubLink = screen.getByText("Github");
-    // expect(githubLink).toBeInTheDocument();
   });
 });
