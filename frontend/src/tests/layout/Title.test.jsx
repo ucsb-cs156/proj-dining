917f0a87a93ab0b7e9897238f955e2e501dff59f
Julia Lin <julia_lin@ucsb.edu>
2025-11-18 20:21:06 -0800

    jml - fixed index.html title test

---
+++ frontend/src/tests/layout/Title.test.jsx
@@ -0,0 +1,15 @@
+import fs from "fs";
+import path from "path";
+import { JSDOM } from "jsdom";
+
+beforeAll(() => {
+  const html = fs.readFileSync(path.resolve("./index.html"), "utf-8");
+  const dom = new JSDOM(html);
+  global.document = dom.window.document;
+  global.window = dom.window;
+});
+
+test("index.html has correct title", async () => {
+  expect(document.title).toBe("UCSB Dining");
+  expect("Vite + React").not.toBeInTheDocument();
+});
