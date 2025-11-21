35901e6d5d15aeb55e84b1509e3ad2aed6886868
Varsha Narasiman <vnarasiman@umail.ucsb.edu>
2025-11-20 23:32:34 -0800

    vn - added the Meals Offered Today column that shows the hyperlinked meals offered in that dining common

--- frontend/src/main/pages/HomePage.jsx
+++ frontend/src/main/pages/HomePage.jsx
@@ -1,5 +1,7 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 import { useBackend } from "../utils/useBackend";
+import { useQueries } from "react-query";
+import axios from "axios";
 import DiningCommonsTable from "../components/DiningCommons/DiningCommonsTable";
 
 export default function HomePage() {
@@ -12,10 +14,31 @@ export default function HomePage() {
 
   const date = new Date().toISOString().split("T")[0];
 
+  const queries = [];
+  if (Array.isArray(data)) {
+    for (const d of data) {
+      queries.push({
+        queryKey: ["meals", d.code, date],
+        queryFn: () =>
+          axios
+            .get(`/api/diningcommons/${date}/${d.code}`)
+            .then((res) => res.data),
+      });
+    }
+  }
+  const mealsOffered = useQueries(queries);
+
+  const combined = Array.isArray(data)
+    ? data.map((d, i) => ({
+        ...d,
+        mealsOfferedToday: mealsOffered[i]?.data ?? [],
+      }))
+    : [];
+
   return (
     <BasicLayout>
       <h1>Dining Commons</h1>
-      <DiningCommonsTable commons={data} date={date} />
+      <DiningCommonsTable commons={combined} date={date} />
     </BasicLayout>
   );
 }
