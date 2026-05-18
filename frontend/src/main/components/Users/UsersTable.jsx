c5d2174fb40e60cf57ad96d324a2daab17aecf7f
Ethan <evoncolln@ucsb.edu>
2026-05-18 15:32:22 -0700

    implement role toggling on admin page, fix and add tests

--- frontend/src/main/components/Users/UsersTable.jsx
+++ frontend/src/main/components/Users/UsersTable.jsx
@@ -1,58 +1,126 @@
 import OurTable from "main/components/OurTable";
-
-const columns = [
-  {
-    Header: "id",
-    accessor: "id", // accessor is the "key" in the data
-  },
-  {
-    Header: "First Name",
-    accessor: "givenName",
-  },
-  {
-    Header: "Last Name",
-    accessor: "familyName",
-  },
-  {
-    Header: "Email",
-    accessor: "email",
-  },
-  {
-    Header: "Admin",
-    id: "admin",
-    accessor: (row, _rowIndex) => String(row.admin), // hack needed for boolean values to show up
-  },
-  {
-    Header: "Moderator",
-    id: "moderator",
-    accessor: (row, _rowIndex) => String(row.moderator),
-  },
-  {
-    Header: "Alias",
-    accessor: "alias",
-  },
-  {
-    Header: "Proposed Alias",
-    accessor: "proposedAlias",
-  },
-  {
-    Header: "Status",
-    accessor: (row) => {
-      if (row.status === "Approved" && row.dateApproved) {
-        // Parse as local date (YYYY-MM-DD)
-        const [year, month, day] = row.dateApproved.split("-");
-        const formattedDate = new Date(
-          year,
-          month - 1,
-          day,
-        ).toLocaleDateString();
-        return `Approved on ${formattedDate}`;
-      }
-      return row.status;
-    },
-  },
-];
+import { useBackendMutation } from "main/utils/useBackend";
+import { toast } from "react-toastify";
 
 export default function UsersTable({ users }) {
+  // helper function to toggle admin status
+  const toggleAdminMutation = useBackendMutation(
+    (row) => ({
+      url: "/api/admin/toggleAdmin",
+      method: "PUT",
+      params: { id: row.id },
+    }),
+    {
+      onSuccess: (_data, row) =>
+        toast("Updated admin status for user " + row.givenName),
+    },
+    // Stryker disable next-line all : don't test internal caching of react query
+    ["/api/admin/users"],
+  );
+  // toggle admin status
+  const handleAdminToggle = (row) => {
+    toggleAdminMutation.mutate(row);
+  };
+
+  // helper function to toggle moderator status
+  const toggleModeratorMutation = useBackendMutation(
+    (row) => ({
+      url: "/api/admin/toggleModerator",
+      method: "PUT",
+      params: { id: row.id },
+    }),
+    {
+      onSuccess: (_data, row) =>
+        toast("Updated moderator status for user " + row.givenName),
+    },
+    // Stryker disable next-line all : don't test internal caching of react query
+    ["/api/admin/users"],
+  );
+  // toggle moderator status
+  const handleModeratorToggle = (row) => {
+    toggleModeratorMutation.mutate(row);
+  };
+
+  // the table
+  const columns = [
+    {
+      Header: "id",
+      accessor: "id",
+    },
+    {
+      Header: "First Name",
+      accessor: "givenName",
+    },
+    {
+      Header: "Last Name",
+      accessor: "familyName",
+    },
+    {
+      Header: "Email",
+      accessor: "email",
+    },
+    {
+      Header: "Admin",
+      id: "admin",
+      accessor: (row) => {
+        return (
+          // Stryker disable next-line StringLiteral
+          <div style={{ display: "flex", gap: "5%", justifyContent: "center" }}>
+            {/* checkbox: toggle admin status */}
+            <input
+              type="checkbox"
+              aria-label="admin"
+              name="adminToggle"
+              checked={row.admin}
+              onChange={() => handleAdminToggle(row)}
+            />
+          </div>
+        );
+      },
+    },
+    {
+      Header: "Moderator",
+      id: "moderator",
+      accessor: (row) => {
+        return (
+          // Stryker disable next-line StringLiteral
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            {/* checkbox: toggle moderator status */}
+            <input
+              type="checkbox"
+              aria-label="moderator"
+              name="moderatorToggle"
+              checked={row.moderator}
+              onChange={() => handleModeratorToggle(row)}
+            />
+          </div>
+        );
+      },
+    },
+    {
+      Header: "Alias",
+      accessor: "alias",
+    },
+    {
+      Header: "Proposed Alias",
+      accessor: "proposedAlias",
+    },
+    {
+      Header: "Status",
+      accessor: (row) => {
+        if (row.status === "Approved" && row.dateApproved) {
+          const [year, month, day] = row.dateApproved.split("-");
+          const formattedDate = new Date(
+            year,
+            month - 1,
+            day,
+          ).toLocaleDateString();
+          return `Approved on ${formattedDate}`;
+        }
+        return row.status;
+      },
+    },
+  ];
+
   return <OurTable data={users} columns={columns} testid={"UsersTable"} />;
 }
--- frontend/src/tests/components/Users/UsersTable.test.jsx
+++ frontend/src/tests/components/Users/UsersTable.test.jsx
@@ -1,18 +1,39 @@
 import { render, screen } from "@testing-library/react";
 import usersFixtures from "fixtures/usersFixtures";
 import UsersTable from "main/components/Users/UsersTable";
+import { within } from "storybook/test";
+import { MemoryRouter } from "react-router";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const mockedNavigate = vi.fn();
+vi.mock("react-router", async () => {
+  const originalModule = await vi.importActual("react-router");
+  return {
+    ...originalModule,
+    useNavigate: () => mockedNavigate,
+  };
+});
+
+const renderWithProviders = (ui) => {
+  const queryClient = new QueryClient();
+  return render(
+    <QueryClientProvider client={queryClient}>
+      <MemoryRouter>{ui}</MemoryRouter>
+    </QueryClientProvider>,
+  );
+};
 
 describe("UserTable tests", () => {
   test("renders without crashing for empty table", () => {
-    render(<UsersTable users={[]} />);
+    renderWithProviders(<UsersTable users={[]} />);
   });
 
   test("renders without crashing for three users", () => {
-    render(<UsersTable users={usersFixtures.threeUsers} />);
+    renderWithProviders(<UsersTable users={usersFixtures.threeUsers} />);
   });
 
   test("Has the expected colum headers and content", () => {
-    render(<UsersTable users={usersFixtures.threeUsers} />);
+    renderWithProviders(<UsersTable users={usersFixtures.threeUsers} />);
 
     const expectedHeaders = [
       "id",
@@ -23,6 +44,7 @@ describe("UserTable tests", () => {
       "Moderator",
       "Alias",
       "Proposed Alias",
+      "Status",
     ];
     const expectedFields = [
       "id",
@@ -33,6 +55,7 @@ describe("UserTable tests", () => {
       "moderator",
       "alias",
       "proposedAlias",
+      "Status",
     ];
     const testId = "UsersTable";
 
@@ -49,25 +72,31 @@ describe("UserTable tests", () => {
     expect(screen.getByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent(
       "1",
     );
+
+    // row 0
+    const row0 = screen.getByTestId(`${testId}-row-0`);
     expect(
-      screen.getByTestId(`${testId}-cell-row-0-col-admin`),
-    ).toHaveTextContent("true");
+      within(row0).getByRole("checkbox", { name: /admin/i }),
+    ).toBeChecked();
     expect(
-      screen.getByTestId(`${testId}-cell-row-0-col-moderator`),
-    ).toHaveTextContent("false");
+      within(row0).getByRole("checkbox", { name: /moderator/i }),
+    ).not.toBeChecked();
+
+    // row 1
+    const row1 = screen.getByTestId(`${testId}-row-1`);
     expect(screen.getByTestId(`${testId}-cell-row-1-col-id`)).toHaveTextContent(
       "2",
     );
     expect(
-      screen.getByTestId(`${testId}-cell-row-1-col-admin`),
-    ).toHaveTextContent("false");
+      within(row1).getByRole("checkbox", { name: /admin/i }),
+    ).not.toBeChecked();
     expect(
-      screen.getByTestId(`${testId}-cell-row-0-col-moderator`),
-    ).toHaveTextContent("false");
+      within(row1).getByRole("checkbox", { name: /moderator/i }),
+    ).toBeChecked();
   });
 
   test("Status column appends approval date only for approved users with a valid date", () => {
-    render(
+    renderWithProviders(
       <UsersTable
         users={[
           { id: 1, status: "Approved", dateApproved: "2024-10-31" },
@@ -82,5 +111,33 @@ describe("UserTable tests", () => {
     expect(screen.getByText("Approved")).toBeInTheDocument();
     expect(screen.getByText("Rejected")).toBeInTheDocument();
     expect(screen.getByText("Awaiting Moderation")).toBeInTheDocument();
+  });
+
+  test("Roles are correctly set on loading the table", () => {
+    renderWithProviders(<UsersTable users={usersFixtures.threeUsers} />);
+
+    const row1 = screen.getByTestId("UsersTable-row-0");
+    const row2 = screen.getByTestId("UsersTable-row-1");
+    const row3 = screen.getByTestId("UsersTable-row-2");
+
+    expect(
+      within(row1).getByRole("checkbox", { name: /admin/i }),
+    ).toBeChecked();
+    expect(
+      within(row1).getByRole("checkbox", { name: /moderator/i }),
+    ).not.toBeChecked();
+    expect(
+      within(row2).getByRole("checkbox", { name: /admin/i }),
+    ).not.toBeChecked();
+    expect(
+      within(row2).getByRole("checkbox", { name: /moderator/i }),
+    ).toBeChecked();
+
+    expect(
+      within(row3).getByRole("checkbox", { name: /admin/i }),
+    ).not.toBeChecked();
+    expect(
+      within(row3).getByRole("checkbox", { name: /moderator/i }),
+    ).not.toBeChecked();
   });
 });
